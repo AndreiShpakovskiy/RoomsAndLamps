6871dd99f8a0bf8bd6e96449dab7faa1f754b533
Andrei Shpakovskiy <shpakovskiyandrei@gmail.com>
2020-07-04 14:29:28 +0300

    Changed path parameter

--- src/main/java/com/shpakovskiy/roomsandlamps/controllers/AllRoomsController.java
+++ src/main/java/com/shpakovskiy/roomsandlamps/controllers/AllRoomsController.java
@@ -21,12 +21,12 @@ public class AllRoomsController {
     }
 
     @RequestMapping("/")
-    private void basicDirection(HttpServletResponse response) {
-        try {
-            response.sendRedirect("/all");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private ModelAndView basicDirection(/*HttpServletResponse response*/) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("all-rooms");
+        modelAndView.addObject("allRooms", roomsService.getAllRooms());
+        modelAndView.addObject("countryService", countryService);
+        return modelAndView;
     }
 
     @RequestMapping("/all")
