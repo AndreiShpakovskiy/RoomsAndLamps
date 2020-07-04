19a1efcc17911d095b375e27731ac970b4846ef7
Andrei Shpakovskiy <shpakovskiyandrei@gmail.com>
2020-07-05 00:23:53 +0300

    Returned changes back

--- src/main/java/com/shpakovskiy/roomsandlamps/controllers/AllRoomsController.java
+++ src/main/java/com/shpakovskiy/roomsandlamps/controllers/AllRoomsController.java
@@ -21,12 +21,17 @@ public class AllRoomsController {
     }
 
     @RequestMapping("/")
-    private void basicDirection(HttpServletResponse response) {
-        try {
-            response.sendRedirect("/all");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private ModelAndView basicDirection(HttpServletResponse response) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("all-rooms");
+        modelAndView.addObject("allRooms", roomsService.getAllRooms());
+        modelAndView.addObject("countryService", countryService);
+        return modelAndView;
+//        try {
+//            response.sendRedirect("/all");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
     @RequestMapping("/all")
