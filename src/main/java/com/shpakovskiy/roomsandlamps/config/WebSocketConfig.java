941e9ccf9f43557b40d6da866933954ea3b62bbc
Andrei Shpakovskiy <shpakovskiyandrei@gmail.com>
2020-07-05 10:43:15 +0300

    Returned changes back

--- src/main/java/com/shpakovskiy/roomsandlamps/config/WebSocketConfig.java
+++ src/main/java/com/shpakovskiy/roomsandlamps/config/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/switch").withSockJS();//HELLO
+        registry.addEndpoint("/switch").setAllowedOrigins("*").withSockJS();//HELLO
     }
 }
